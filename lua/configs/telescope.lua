ad397e463b73639c28b324ad60d8a67679859ae1
Saar Tako <saart@d-fendsolutions.com>
2026-06-11 15:51:45 +0300

    show hidden files as well in telescope

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,3 +1,21 @@
 return {
-
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden", -- This flag tells ripgrep to search dotfiles
+        },
+        -- Ensure .git folders are ignored so you don't grep through git history
+        file_ignore_patterns = { "node_modules", ".git/" },
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+    },
 }
