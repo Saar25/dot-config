ed0e28b04b01d559cda363b5c6d4fe2b21c1fd14
Saar Tako <saartako25@gmail.com>
2026-07-22 13:36:13 +0300

    turn on nvim relative number

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,10 +3,13 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.tabstop = 4
 o.expandtab = true
+o.smartindent = true
+o.tabstop = 4
 o.softtabstop = 4
 o.shiftwidth = 4
 
 local opt = vim.opt
 opt.clipboard = "unnamedplus"
+opt.relativenumber = true
+opt.number = true
