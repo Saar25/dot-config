7c51c80510b6e4dd6f1fe48170e8b77acaa354ce
Saar Tako <saartako25@gmail.com>
2026-07-29 09:45:08 +0300

    add telescope resume mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,8 +1,10 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+
+-- =======
+-- General
+-- =======
 
 map("n", ";", ":", {
     desc = "CMD enter command mode",
@@ -14,18 +16,36 @@ map("n", "<leader>e", ':lua vim.diagnostic.open_float(0, {scope="line"})<CR>', {
     desc = "Show error",
 })
 
+-- ========
+-- Markdown
+-- ========
+
 map("n", "<leader>mp", ":MarkdownPreview<CR>", {
     desc = "Markdown Preview",
 })
 
-map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", {
-    desc = "telescope find references",
+-- =====
+-- Noice
+-- =====
+
+map("n", "<leader>ad", "<cmd>Noice dismiss<CR>", {
+    desc = "noice dismiss",
     noremap = true,
     silent = true,
 })
 
-map("n", "<leader>ad", "<cmd>Noice dismiss<CR>", {
-    desc = "noice dismiss",
+-- =========
+-- Telescope
+-- =========
+
+map("n", "<leader>fR", "<cmd>Telescope resume<CR>", {
+    desc = "telescope resume",
+    noremap = true,
+    silent = true,
+})
+
+map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", {
+    desc = "telescope find references",
     noremap = true,
     silent = true,
 })
@@ -42,6 +62,10 @@ map("n", "<leader>fw", function()
     }
 end, { desc = "Telescope Live Grep" })
 
+-- ==========
+-- Treesitter
+-- ==========
+
 map("x", "v", function()
     if vim.treesitter.get_parser(nil, nil, { error = false }) then
         require("vim.treesitter._select").select_parent(vim.v.count1)
@@ -50,7 +74,6 @@ map("x", "v", function()
     end
 end, { desc = "Selection increment " })
 
--- 3. SHRINK SELECTION with 'V' (Shift+v) in Visual Mode
 map("x", "V", function()
     if vim.treesitter.get_parser(nil, nil, { error = false }) then
         require("vim.treesitter._select").select_child(vim.v.count1)
@@ -58,6 +81,10 @@ map("x", "V", function()
         vim.lsp.buf.selection_range(-vim.v.count1)
     end
 end, { desc = "Selection decrement" })
+
+-- =======
+-- Conform
+-- =======
 
 map("n", "<leader>fm", function()
     -- 1. Format the file using conform
